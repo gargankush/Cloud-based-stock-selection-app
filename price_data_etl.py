9708f07b2ac15cef6d881e312173929c3ca0a730
rennzz <niyazi.eren@hotmail.fr>
2020-10-24 15:29:43 +0200

    added EMR etl scripts

---
+++ price_data_etl.py
@@ -0,0 +1,70 @@
+import boto3
+import requests
+import yaml
+import json
+import time
+from datetime import date, timedelta, datetime
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+
+
+if __name__ == "__main__":
+    
+    def alphavantage_api_call(api_key, symbol, interval="1min"):
+        args = f"function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={interval}&apikey={api_key}&outputsize=full"
+        url = f"https://www.alphavantage.co/query?{args}"
+        response = requests.get(url)
+        return response
+
+    bucket_name = "team166project"
+    spark = SparkSession\
+        .builder\
+        .appName("etl")\
+        .getOrCreate()
+   
+    s3 = boto3.client('s3')
+    yaml_file = s3.get_object(Bucket="cse6242-neren3", Key="config.yaml")
+    yaml_file = yaml.safe_load(yaml_file["Body"].read().decode("utf-8"))
+    api_key = yaml_file["alphavantage"]["api_key"]
+    body = s3.get_object(Bucket=bucket_name, Key="symbols.txt")['Body'].read()
+    symbols = body.decode("utf8").split('\n')
+    
+    schema = StructType([
+    StructField("symbol", StringType(), True),
+    StructField("timeframe", StringType(), True), 
+    StructField("open", StringType(), True),
+    StructField("high", StringType(), True),
+    StructField("low", StringType(), True),
+    StructField("close", StringType(), True),
+    StructField("volume", StringType(), True)
+    ])
+    
+    row = []
+    yesterday = (date.today() - timedelta(days=1)).isoformat()
+    today = date.today().isoformat()
+    
+    old_df = spark.read.csv('s3://' + bucket_name + '/price-data-' + yesterday + '/*', header=True, inferSchema=True)
+    
+    for symbol in symbols:
+        try:
+            price_data = alphavantage_api_call(api_key, symbol)
+            time.sleep(15)
+            price_data = json.loads(price_data.text)["Time Series (1min)"]
+            for tf, price in price_data.items():
+                row.append((symbol, 
+                           tf, 
+                           price["1. open"], 
+                           price["2. high"], 
+                           price["3. low"],
+                           price["4. close"],
+                           price["5. volume"]))
+        except:
+            continue
+
+    new_df = spark.createDataFrame(row, schema)
+    df = new_df.union(old_df)
+    df.repartition(1).write.csv('s3://' + bucket_name + '/price-data-' + today, header=True)
+    # delete yesterday's data
+    response = s3.list_objects(Bucket=bucket_name, Prefix="price-data-" + yesterday)
+    for obj in response["Contents"]:
+        s3.delete_object(Bucket=bucket_name, Key=obj["Key"])
---
+++ twitter_data_etl.py
@@ -0,0 +1,74 @@
+import boto3
+import requests
+import yaml
+import json
+import time
+import random
+import datetime
+from datetime import date, timedelta, datetime
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+
+if __name__ == "__main__":
+    
+    def twitter_api_call(api_key, symbol, start_time, end_time, max_results=100):
+        args = f"max_results={max_results}&query={symbol}&start_time={start_time}&end_time={end_time}"
+        url = f"https://api.twitter.com/2/tweets/search/recent?{args}"
+        headers = {"Authorization": "Bearer {}".format(api_key)}
+        response = requests.request("GET", url, headers=headers)
+        return response.json()
+
+    def extract_tweets(twitter_json):
+        tweets = []
+        for i in range(len(twitter_json["data"])):
+            tweets.append(twitter_json["data"][i]["text"])
+        return tweets
+
+    s3 = boto3.client("s3")
+    yaml_file = s3.get_object(Bucket="cse6242-neren3", Key="config.yaml")
+    yaml_file = yaml.safe_load(yaml_file["Body"].read().decode("utf-8"))
+    api_key = yaml_file["twitter"]["bearer_token"]
+
+    bucket_name = "team166project"
+    spark = SparkSession\
+            .builder\
+            .appName("etl")\
+            .getOrCreate()
+
+    body = s3.get_object(Bucket=bucket_name, Key="symbols.txt")['Body'].read()
+    symbols = body.decode("utf8").split('\n')
+
+    schema = StructType([
+    StructField("symbol", StringType(), True),
+    StructField("date", StringType(), True), 
+    StructField("tweets", StringType(), True)
+    ])
+
+    row = []
+    yesterday = (date.today() - timedelta(days=1)).isoformat()
+    today = date.today().isoformat()
+    week_ago = (date.today() - timedelta(days=6)).isoformat()
+    
+    start_time = (datetime.utcnow() - timedelta(days=1)).isoformat("T") + "Z" 
+    end_time = (datetime.utcnow()- timedelta(hours=1)).isoformat("T") + "Z"
+    old_df = spark.read.json('s3://' + bucket_name + '/twitter-data-' + yesterday + '/*')
+    random.shuffle(symbols)
+
+    for symbol in symbols[:450]:
+        try:
+            twitter_json = twitter_api_call(api_key, symbol, start_time, end_time, max_results=100)
+            tweets = extract_tweets(twitter_json)
+            for tweet in tweets:
+                  row.append((symbol, today, tweet))
+        except:
+            continue
+        
+    new_df = spark.createDataFrame(row, schema)
+    df = new_df.union(old_df)
+    # keep data for trailing week
+    df = df.filter(df["date"] != week_ago)
+    df.repartition(1).write.json('s3://' + bucket_name + '/twitter-data-' + today)
+    # delete old files
+    response = s3.list_objects(Bucket=bucket_name, Prefix="twitter-data-" + yesterday)
+    for obj in response["Contents"]:
+        s3.delete_object(Bucket=bucket_name, Key=obj["Key"])
